ebb27c7be30d006d9a678e8b13cdc4ebf42fd669
zulimazuli <zulimazuli@gmail.com>
2020-05-10 19:27:36 +0200

    Simple Sender-Receiver

---
+++ Receiver/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Receiver started.");
+            Receive();
+        }
+
+        private static void Receive()
+        {
+            var factory = new ConnectionFactory {HostName = "localhost"};
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            //might be redundant
+            channel.QueueDeclare("hello", false, false, false, null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var msg = Encoding.UTF8.GetString(body.ToArray());
+                Console.WriteLine(" [x] Received: {0}", msg);
+            };
+
+            channel.BasicConsume(queue: "hello", true, consumer);
+            
+            Console.ReadLine(); //wait for messages
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Sender started. Enter messages to send.");
+            while (true)
+            {
+                var msg = Console.ReadLine();
+                Send(msg);
+            }
+        }
+
+        private static void Send(string message)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish("", "hello", null, body);
+            Console.WriteLine($" [x] Sent: {message}");
+        }
+    }
+}
